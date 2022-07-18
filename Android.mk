b34328cb338f92ccef31063cf4394478fa100ac9
jmpfbmx <jmpf.bmx@gmail.com>
2022-07-18 21:37:05 +0200

    libselinux: Import MediaTek changes
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I49066aea40fa37fc52ae84047e1e1654a871de78

--- Android.mk
+++ Android.mk
@@ -52,6 +52,7 @@ LOCAL_C_INCLUDES := external/pcre
 LOCAL_WHOLE_STATIC_LIBRARIES := libpcre
 # 1003 corresponds to auditd, from system/core/logd/event.logtags
 LOCAL_CFLAGS := -DAUDITD_LOG_TAG=1003
+LOCAL_MULTILIB := both
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
